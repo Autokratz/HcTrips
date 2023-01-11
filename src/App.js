bc7f2300892e503373e386d480a80294e272dd1f
Hector Cabra <hectorcabra95@gmail.com>
2023-01-11 16:04:29 -0500

    Workshop week 2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import React from 'react';
+import {Routes, Route} from "react-router-dom";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import './App.css';
-
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 function App() {
     return (
-        <div className='App'>
-            <Header />
+        <div className="App">
+            <Header/>
             <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-                <Route 
-                path='directory/:campsiteId'
-                element={<CampsiteDetailPage />}
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='contact' element={<ContactPage/>}/>
+                <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage/>}
                 />
+                <Route path='about' element={<AboutPage/>}/>
             </Routes>
-            <Footer />
+            <Footer/>
         </div>
     );
 }
-
 export default App;
