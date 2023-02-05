5b463b2149427548561c754c1571e7b0f281ef9a
Hector Cabra <hectorcabra95@gmail.com>
2023-02-05 00:27:33 -0500

    Async Partners

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+import React from 'react';
 import {Routes, Route} from "react-router-dom";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import './App.css';
-import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
-import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import {fetchCampsites} from "./features/campsites/campsitesSlice";
+import {fetchPartners} from "./features/partners/partnersSlice";
 
 function App() {
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(fetchCampsites());
-
+        dispatch(fetchPartners());
     }, [dispatch]);
 
     return (
         <div className="App">
             <Header/>
             <Routes>
-                <Route path='/' element={<HomePage/>}/>
-                <Route path='contact' element={<ContactPage/>}/>
-                <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="contact" element={<ContactPage/>}/>
+                <Route path="directory" element={<CampsitesDirectoryPage/>}/>
                 <Route
-                    path='directory/:campsiteId'
+                    path="directory/:campsiteId"
                     element={<CampsiteDetailPage/>}
                 />
-                <Route path='about' element={<AboutPage/>}/>
+                <Route path="about" element={<AboutPage/>}/>
             </Routes>
             <Footer/>
         </div>
